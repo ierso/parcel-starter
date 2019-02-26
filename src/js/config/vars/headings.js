b69d411d145baee93c91f38a3898b6fa9904528a
Cory <coryp@jkdesign.com>
2019-02-26 17:05:32 -0500

    Update typography mixin. Add different heading breakpoints.

--- src/js/config/vars/headings.js
+++ src/js/config/vars/headings.js
@@ -1,8 +1,36 @@
 module.exports = {
-  $gH1: "4.209rem",
-  $gH2: "3.157rem",
-  $gH3: "2.369rem",
-  $gH4: "1.777rem",
-  $gH5: "1.333rem",
-  $gH6: "1rem"
+  $gH1: "3.244rem",
+  $gH2: "2.883rem",
+  $gH3: "2.563rem",
+  $gH4: "2.278rem",
+  $gH5: "2.025rem",
+  $gH6: "1.8rem",
+
+  $gMobileH1: "3.649rem",
+  $gMobileH2: "3.243rem",
+  $gMobileH3: "2.883rem",
+  $gMobileH4: "2.562rem",
+  $gMobileH5: "2.278rem",
+  $gMobileH6: "2.025",
+
+  $gMobileLgH1: "4.105rem",
+  $gMobileLgH2: "3.862rem",
+  $gMobileLgH3: "3.243rem",
+  $gMobileLgH4: "2.882rem",
+  $gMobileLgH5: "2.562rem",
+  $gMobileLgH6: "2.278rem",
+
+  $gTabletH1: "4.618rem",
+  $gTabletH2: "4.344rem",
+  $gTabletH3: "3.648rem",
+  $gTabletH4: "3.242rem",
+  $gTabletH5: "2.882rem",
+  $gTabletH6: "2.562rem",
+
+  $gDesktopH1: "5.195rem",
+  $gDesktopH2: "4.887rem",
+  $gDesktopH3: "4.104rem",
+  $gDesktopH4: "3.647rem",
+  $gDesktopH5: "3.242rem",
+  $gDesktopH6: "2.882rem"
 };
